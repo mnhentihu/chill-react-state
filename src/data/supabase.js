924184c01e01990cb5c0bf1f5e549ce5630994b8
Natsir <hentihu.natsir@gmail.com>
2025-03-23 20:07:27 +0700

    feat: add dotenv

--- src/data/supabase.js
+++ src/data/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://hrrkbuoicwqaipmblcie.supabase.co";
-const ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhycmtidW9pY3dxYWlwbWJsY2llIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0NTYzNjEsImV4cCI6MjA1ODAzMjM2MX0.lGKQ46dyhxlzejfvZ74Wx6BoTHw8dHMGEx5prxjRlqo";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(SUPABASE_URL, ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseKey);
